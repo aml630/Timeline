07dd26adcdf318623cbaea5b95712921f02c54aa
alex <alex.m.larson@gmail.com>
2017-01-16 19:57:47 -0800

    initial commit

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeLineBlog.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: Post
+        public ActionResult Timeline()
+        {
+            return View();
+        }
+
+        public ActionResult OrangeGraph()
+        {
+
+            return View();
+        }
+
+        public ActionResult GraphDataCard()
+        {
+
+            return View();
+        }
+
+        public ActionResult TransparentBarGraph()
+        {
+
+            return View();
+        }
+
+        public ActionResult ColorfulDataPoints()
+        {
+
+            return View();
+        }
+    }
+}
